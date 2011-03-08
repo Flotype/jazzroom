c9812658e3cd0fc8836482b126bb3fb39bb342aa
ericz <really.ez@gmail.com>
2011-03-08 01:45:02 -0800

    jazzy

--- js/jazz.js
+++ js/jazz.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
   
-  
+  setInterval(function(){
+    
+    var mark = $("<div></div>").addClass("mark");
 
+    $("#disp").append(mark);
+    
+    mark.animate({left: "696px"}, 3000, 'linear', function(){
+      $(this).fadeOut(200);
+    });
+  
+  }, 2000);
+  
 });
+
+function createNote(type) {
+
+  var note = $("<div></div>").addClass("note");
+
+  if(type == 'piano') {
+    note.css("top", "50px");
+  } else if(type == 'bass') {
+    note.css("top", "190px");
+  } else if(type == 'drum') {
+    note.css("top", "325px");
+  }  
+  
+  note.animate({left: "696px"}, 3000, 'linear', function(){
+      $(this).fadeOut(200);
+  });
+  
+  $("#disp").append(note);
+  
+}
