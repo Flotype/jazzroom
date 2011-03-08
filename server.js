1026ae8a14527ace492654d589e97724cb5e700b
Sridatta Thatipamala <Sridatta Thatipamala>
2011-03-08 02:59:21 -0800

    oh god this is bad

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var server = require('http').createServer(function(req, response){
-  fs.readFile('./index.html', function(err, data){
+  fs.readFile('.'+ req.url, function(err, data){
     response.writeHead(200);  
     response.write(data);  
     response.end();
